58708fae68c56410496cd2b9847c931dc6291e2b
27kan <79520951+27kan@users.noreply.github.com>
2021-04-02 17:38:27 +0900

    AiaxAppの準備

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,13 +1,11 @@
 class PostsController < ApplicationController
-  def index # PostというDBからidを代入
-    @posts = Post.all
-  end
-  
-  def new
+  def index
+    @posts = Post.all.order(id: "DESC")
   end
 
   def create
     Post.create(content: params[:content])
+    redirect_to action: :index
   end
   
 end
