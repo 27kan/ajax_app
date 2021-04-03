3328d6d26c3cd3f8635b8e6af07dad488bf51a2d
27kan <79520951+27kan@users.noreply.github.com>
2021-04-04 00:34:07 +0900

    サーバーサイドの実装
    ・ルーティング設定
    ・checkedアクション定義

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -7,5 +7,19 @@ class PostsController < ApplicationController
     Post.create(content: params[:content])
     redirect_to action: :index
   end
+
+  def checked
+    post = Post.find(params[:id])
+    if post.checked # 取得したメモが既読⇆未読を反転する
+      post.update(checked: false)
+    else
+      post.update(checked: true)
+    end
+
+    item = Post.find(params[:id])
+    # 既読⇆未読を反転させた値を再取得してitemに代入
+    render json: { post: item }
+    # 既読処理を行なった値をレスポンスとして返す
+  end
   
 end
